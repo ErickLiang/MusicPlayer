22e4c0f0ddd4709130f987cf114fd9fe41f12a8c
ErickLiang <290727629@qq.com>
2020-06-23 13:43:46 +0800

    Update musicListFrame.py
    增加了返回选中的歌曲

--- musicListFrame.py
+++ musicListFrame.py
@@ -4,15 +4,37 @@ from tkinter import ttk
 
 class MusicListFrame():
     def __init__(self, master, musicResoucePath):
+        # 增加左侧frame，为歌单显示
         self.frame = tkinter.Frame(master)
         self.frame.grid(row=0, column=0)
 
+        # 新的树
         self.tree = ttk.Treeview(self.frame)
-        self.tree.pack()
+        self.tree.pack(side=tkinter.LEFT, fill=tkinter.Y)
         root = self.tree.insert('', 'end', text=self.getMusicList(musicResoucePath), open=True)
 
+        # 增加分支
         self.loadSong(root, musicResoucePath)
 
+        # 增加滚动条
+        self.scrollbar = tkinter.Scrollbar(self.frame)
+        self.scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        self.scrollbar.config(command=self.tree.yview)
+
+        self.tree.config(yscrollcommand=self.scrollbar.set)
+
+        # 绑定事件：分支选中，
+        self.tree.bind("<<TreeviewSelect>>", self.getSelectedSong)
+
+    # 返回被选中的值
+    def getSelectedSong(self, event):
+        self.v = event.widget.selection()
+        for sv in self.v:
+            file = self.tree.item(sv)['text']
+            print(file)
+            return file
+
+    # 路径处理，作为分支的值显示
     def getMusicList(self, musicResoucePath):
         return os.path.split(musicResoucePath)[-1]
 
